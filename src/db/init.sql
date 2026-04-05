9ea8120d063b47638697a840ccf4787291a0df2e
JabloZ <jablozkontakt@gmail.com>
2026-04-05 23:08:44 +0200

    init scripts

--- src/db/init.sql
+++ src/db/init.sql
@@ -4,8 +4,42 @@ CREATE TABLE Dwarfs(
     id INT PRIMARY KEY,
     name VARCHAR(50),
     volume INT,
-    depositAssigned BOOLEAN
+    depositAssigned BOOLEAN,
+    FOREIGN KEY house_id REFERENCES House(id)
 );
-
+CREATE TABLE Minerals(
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+)
+CREATE TABLE Preferences(
+    id INT PRIMARY KEY,
+    FOREIGN KEY dwarf_id REFERENCES Dwarfs(id)
+    FOREIGN KEY mineral_id REFERENCES Minerals(id)
+    multiplier FLOAT
+);
+CREATE TABLE Deposit(
+    id INT PRIMARY KEY,
+    FOREIGN KEY mineral_id REFERENCES Minerals(id),
+    capacity INT,
+    x INT,
+    y INT
+)
+CREATE TABLE DepositDwarfs(
+    FOREIGN KEY dwarf_id REFERENCES Dwarfs(id),
+    FOREIGN KEY deposit_id REFERENCES Deposit(id),
+)
+CREATE TABLE House(
+    id INT PRIMARY KEY,
+    x INT,
+    y INT,
+)
+CREATE TABLE Map(
+    size_x INT,
+    size_y INT
+)
+CREATE TABLE Distances(
+    FOREIGN KEY house_id REFERENCES House(id),
+    FOREIGN KEY deposit_id REFERENCES Deposit(id),
+)
 INSERT INTO Dwarfs VALUES (1,'Kamin',70,false);
 INSERT INTO Dwarfs VALUES (2,'Zorak',30,false);
