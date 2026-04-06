b28e3535aee10c6f2c098aeb7f3b80a7eb7289aa
kamilhys <kamilhys@mat.umk.pl>
2026-04-06 21:23:43 +0200

    Iss2 (Dostosowanie modelu do bazy SQL, hermetyzacja danych i szkielet algorytmu grafowego) (#7)
    * deleted anonim
    
    * docker compose for c#
    
    * .vscode gitignore
    
    * fix .vscode
    
    * readme edit
    
    * pliki z klasami i metodami (V1)
    
    * Dostosowanie plików do pracy z bazą danych
    
    * Refactor: hermetyzacja encji i dodanie szkieletu grafu pod algorytm przepływów
    
    * Zbędne komentarze (nie wiem czemu się zachowały)
    
    * fix: naprawa budowania w Dockerze i aktualizacja struktury Mapy
    
    ---------
    
    Co-authored-by: JabloZ <jablozkontakt@gmail.com>

---
+++ src/backend/krasnoludki/Entities/Deposit.cs
@@ -0,0 +1,23 @@
+namespace krasnoludki.Entities
+{
+    public class Deposit
+    {
+        public int Id { get; set; }
+        public int MineralId { get; set; }
+        public int Capacity { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        // Właściwość nawigacyjna
+        public Mineral? Mineral { get; set; } 
+
+        public Deposit(int id, int mineralId, int capacity, int x, int y)
+        {
+            Id = id;
+            MineralId = mineralId;
+            Capacity = capacity;
+            X = x;
+            Y = y;
+        }
+    }
+}
