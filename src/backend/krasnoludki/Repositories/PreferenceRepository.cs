10645e3d0fcebfc6789979a5a8f1dabddbd1c1c6
JabloZ <jablozkontakt@gmail.com>
2026-04-20 17:09:24 +0200

    finalne poprawki przed mergem z mainem

--- src/backend/krasnoludki/Repositories/PreferenceRepository.cs
+++ src/backend/krasnoludki/Repositories/PreferenceRepository.cs
@@ -8,11 +8,13 @@ namespace krasnoludki.Repositories
     {
         public async Task<List<Preference>> GetPreferences()
         {
-            var cur=new DatabaseConn();
+            //niedokonczone
+            var cur = new DatabaseConn();
             using var conn = await cur.DbConnect();
-            const string command= "SELECT * from Houses";
+            const string command = "SELECT * from Houses";
             var preferences = await conn.QueryAsync<Preference>(command);
             return preferences.ToList();
         }
+        
     }
 }
