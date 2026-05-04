a4211e1104f75a72c9b71dff8164ede7f4b87b01
JabloZ <jablozkontakt@gmail.com>
2026-05-04 13:15:37 +0200

    integracja testow, pierwszy test do algorytmu nr1

---
+++ src/backend/krasnoludki.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+using System.Collections.Generic;
+using System.Linq;
+using krasnoludki.Entities;
+using krasnoludki.Algorithms;
+namespace krasnoludki.Tests
+{
+    public class AlgorithmTest1
+    {
+        [Fact]
+        public void SolveAssignments_WhenValidDwarfAndValidDeposit_AssignDwarfToDeposit() 
+        {
+            //test przypisania krasnoluda do zloza
+            var service = new AssignmentSolver();
+            var house = new House {Id=100, X=0, Y=0};
+            var dwarf = new Dwarf(id: 1, name: "Gimli", loudness: 10, depositAssigned: false, houseId: 100)
+            {
+                House=house,
+                preferences = new Dictionary<int, float> {{ 1, 1.0f },{ 2, 0.0f}}
+            };
+            var deposit = new Deposit(id: 50, mineralId: 1, capacity: 100, x: 10, y: 20);
+            var dwarfs = new List<Dwarf> { dwarf };
+            var deposits = new List<Deposit> { deposit };
+            service.SolveAssignments(dwarfs, deposits);
+            Assert.NotNull(dwarf.Deposit);
+            Assert.Equal(50, dwarf.Deposit.Id);
+        }
+        [Fact]
+        public void SolveAssignments_WhenDwarfsAndDepositsAreValid_ReturnCorrectAssignment()
+        {
+            
+        }
+        [Fact]
+        public void SolveAssignments_WhenDwarfsEmpty_ReturnEmptyDictionary()
+        {
+            
+        }
+        [Fact]
+        public void SolveAssignments_WhenDepositsEmpty_ReturnEmptyDictionary()
+        {
+            
+        }
+    }
+}
