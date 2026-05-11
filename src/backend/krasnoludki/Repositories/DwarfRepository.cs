4dd61366c341300066e5354efd9b0a042fcadcff
JabloZ <jablozkontakt@gmail.com>
2026-05-11 23:48:31 +0200

    demo zmiany - jak widac dwarf normalnie dziala po odczytaniu z pliku, wiec w bazie mozemy trzymac sciezki i tyle wystarczy

--- src/backend/krasnoludki/Repositories/DwarfRepository.cs
+++ src/backend/krasnoludki/Repositories/DwarfRepository.cs
@@ -1,38 +1,63 @@
-using Dapper;
-using krasnoludki.db;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using krasnoludki.Entities;
 
-namespace krasnoludki.Repositories{
+namespace krasnoludki.Repositories
+{
     public class DwarfRepository
     {
+        private const string DwarfsFilePath = "test_data/dwarfs.csv";
+        private const string DepositsFilePath = "deposits.csv";
 
         public async Task<List<Dwarf>> GetDwarfs()
         {
-            var Cur=new DatabaseConn();
-            using var Conn = await Cur.DbConnect();
-            const string Command= "SELECT Id, Name, Loudness, DepositAssigned, houseId, depositId from Dwarfs";
-            var Dwarfs = await Conn.QueryAsync<Dwarf>(Command);
-        
-            return Dwarfs.ToList();
+            string[] lines = await File.ReadAllLinesAsync(DwarfsFilePath);
+            List<Dwarf> dwarfs = new List<Dwarf>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] columns = lines[i].Split(',');
+
+                int? dId = null;
+                if (!string.IsNullOrEmpty(columns[5]))
+                {
+                    dId = int.Parse(columns[5]);
+                }
+
+                Dwarf dwarf = new Dwarf
+                {
+                    Id = int.Parse(columns[0]),
+                    Name = columns[1],
+                    Loudness = int.Parse(columns[2]),
+                    DepositAssigned = bool.Parse(columns[3]),
+                    HouseId = int.Parse(columns[4]),
+                    DepositId = dId
+                };
+
+                dwarfs.Add(dwarf);
+            }
+
+            return dwarfs;
+        }
+
+        public async Task<int> GetMineralIdByDepositId(int depositId)
+        {
+            string[] lines = await File.ReadAllLinesAsync(DepositsFilePath);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] columns = lines[i].Split(',');
+                int currentId = int.Parse(columns[0]);
+
+                if (currentId == depositId)
+                {
+                    return int.Parse(columns[1]);
+                }
+            }
+
+            return 0;
         }
     }
 }
-/*
-CREATE TABLE Dwarfs(
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    volume INT,
-    deposit_assigned BOOLEAN,
-    house_id INT REFERENCES House(id),
-    deposit_id INT NULL REFERENCES Deposits(id) 
-);
-            var Cur = new DatabaseConn();
-            using var Conn = await Cur.DbConnect();
-
-            var P = new DynamicParameters();
-            P.Add("p", Deposit.Id);
-
-            const string Command = "SELECT mineral_id FROM Deposits WHERE id = @p";
-            int Result = await Conn.QueryFirstOrDefaultAsync<int>(Command, P);
-            return Result;
-*/
