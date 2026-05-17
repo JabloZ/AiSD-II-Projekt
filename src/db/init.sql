9141ae80f5e0869f8233553188f1837ecca754b7
JabloZ <jablozkontakt@gmail.com>
2026-05-17 18:36:55 +0200

    przykladowe dane do bazy

--- src/db/init.sql
+++ src/db/init.sql
@@ -31,8 +31,20 @@ CREATE TABLE Map(
 --    length INT
 --)
 
-INSERT INTO Houses VALUES (1,'test_data/houses.csv');
-INSERT INTO Dwarfs VALUES (1,'test_data/dwarfs.csv');
-INSERT INTO Deposits VALUES (1,'test_data/deposits.csv');
-INSERT INTO Minerals VALUES (1,'test_data/minerals.csv');
-INSERT INTO Preferences VALUES (1,'test_data/preferences.csv');
+INSERT INTO Houses VALUES (1, 40, 50);
+INSERT INTO Houses VALUES (2, 50, 50);
+
+INSERT INTO Minerals VALUES (1,'Złoto');
+INSERT INTO Minerals VALUES (2,'Srebro');
+
+INSERT INTO Deposits VALUES (1,1,10,50,80);
+INSERT INTO Deposits VALUES (2,2,12,100,20);
+
+INSERT INTO Dwarfs VALUES (1,'Kamin',70,true,1,1);
+INSERT INTO Dwarfs VALUES (2,'Zorak',30,true,2,2);
+INSERT INTO Dwarfs VALUES (3,'Karoz',40,false,2,NULL);
+
+INSERT INTO Preferences VALUES (1, 1, 0.3);
+INSERT INTO Preferences VALUES (1, 2, 1);
+INSERT INTO Preferences VALUES (2, 1, 1);
+INSERT INTO Preferences VALUES (2, 2, 0.5);
